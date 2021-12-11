6e0b094600f8463654f0db51f87342868b19d92a
Gülay Yolcu <28225625+gulayyolcu@users.noreply.github.com>
2021-12-11 19:31:51 +0300

    Add files via upload

---
+++ src/routes/Faq.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/footer/Footer'
+import Navbar from '../components/navbar/Navbar'
+import FaqSection from '../components/faq/Faq'
+
+const Faq = () => {
+    return (
+        <>
+            <Navbar/>
+            <FaqSection/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Faq
---
+++ src/routes/Pricing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/footer/Footer'
+import Navbar from '../components/navbar/Navbar'
+import PricingSection from '../components/pricing/Pricing'
+
+const Pricing = () => {
+    return (
+        <>
+            <Navbar/>
+            <PricingSection/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Pricing
